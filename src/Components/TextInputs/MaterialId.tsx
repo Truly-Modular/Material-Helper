2eb0d972e1096788ccbec4fc7b31ee081bcdc5e6
Skiftstar <yukamitft@gmail.com>
2024-07-22 17:35:56 +0200

    move color picker

--- src/Components/TextInputs/MaterialId.tsx
+++ src/Components/TextInputs/MaterialId.tsx
@@ -1,46 +1,57 @@
-import React, { useState, ChangeEvent } from 'react';
+import React, { useState, ChangeEvent } from "react"
 
 interface MaterialEntryProps {
-  onSubmit: (entryText: string) => void;
+	onSubmit: (entryText: string) => void
 }
 
 const MaterialEntry: React.FC<MaterialEntryProps> = ({ onSubmit }) => {
-  const [entryText, setEntryText] = useState('');
+	const [entryText, setEntryText] = useState("")
 
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    // Validate input as the user types
-    const inputValue = e.target.value;
-    const regex = /^[a-z_:1-9A-Z]+$/;
+	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		// Validate input as the user types
+		const inputValue = e.target.value
+		const regex = /^[a-z_:1-9A-Z]+$/
 
-    if (regex.test(inputValue) || inputValue === '') {
-      setEntryText(inputValue.toLowerCase());
-      onSubmit(inputValue.toLowerCase()); // Optionally submit on every key press
-    }
-  };
+		if (regex.test(inputValue) || inputValue === "") {
+			setEntryText(inputValue.toLowerCase())
+			onSubmit(inputValue.toLowerCase()) // Optionally submit on every key press
+		}
+	}
 
-  return (
-    <div className="entry" style={{ position: 'relative', backgroundColor: 'var(--discord-gray-2)', borderRadius: '8px', padding: '10px' }}>
-      <label htmlFor="materialInput" style={{ display: 'block', color: 'var(--discord-white)', marginBottom: '5px' }}>
-        Material Item ID
-      </label>
-      <input
-        type="text"
-        id="materialInput"
-        value={entryText}
-        onChange={handleInputChange}
-        placeholder="modid:item_id"
-        style={{
-          backgroundColor: 'var(--discord-gray-3)',
-          color: 'var(--discord-white)',
-          border: 'none',
-          width: '90%',
-          padding: '10px',
-          borderRadius: '8px',
-          outline: 'none', // Remove default focus outline
-        }}
-      />
-    </div>
-  );
-};
+	return (
+		<div
+			className="entry special-text"
+			style={{
+				position: "relative",
+				backgroundColor: "var(--discord-gray-2)",
+				borderRadius: "8px",
+				padding: "10px",
+			}}
+		>
+			<label
+				htmlFor="materialInput"
+				style={{ display: "block", color: "var(--discord-white)", marginBottom: "5px" }}
+			>
+				Material Item ID
+			</label>
+			<input
+				type="text"
+				id="materialInput"
+				value={entryText}
+				onChange={handleInputChange}
+				placeholder="modid:item_id"
+				style={{
+					backgroundColor: "var(--discord-gray-3)",
+					color: "var(--discord-white)",
+					border: "none",
+					width: "90%",
+					padding: "10px",
+					borderRadius: "8px",
+					outline: "none", // Remove default focus outline
+				}}
+			/>
+		</div>
+	)
+}
 
-export default MaterialEntry;
+export default MaterialEntry
